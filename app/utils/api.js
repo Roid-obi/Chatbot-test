1c059c924cebc677e1aff4209ce8fc6cbf4efc51
Roid-obi <roidrobih@gmail.com>
2025-06-29 00:41:14 +0700

    update: get api

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,12 +1,10 @@
 export const fetchQwenResponse = async (userInput) => {
-    const OPENROUTER_API_KEY = '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'; // contohdiamazon@
-    // const OPENROUTER_API_KEY = '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'; // hiloapukat@
-    // const OPENROUTER_API_KEY = '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'; // roidrobih@
+    const OPENROUTER_API_KEY = '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'; // hiloapukat@
     const YOUR_SITE_URL = 'https://www.yoursite.com'; // Ganti dengan URL situs Anda
     const YOUR_SITE_NAME = 'Your Site Name'; // Ganti dengan nama situs Anda
 
     try {
-        const response = await fetch('https://openrouter.ai/api/v1/chat/completions ', {
+        const response = await fetch('https://openrouter.ai/api/v1/chat/completions', {
             method: 'POST',
             headers: {
                 Authorization: `Bearer ${OPENROUTER_API_KEY}`,
@@ -15,7 +13,7 @@ export const fetchQwenResponse = async (userInput) => {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                model: 'deepseek/deepseek-r1-0528-qwen3-8b:free',
+                model: 'qwen/qwen2.5-vl-72b-instruct:free',
                 messages: [
                     {
                         role: 'user',
